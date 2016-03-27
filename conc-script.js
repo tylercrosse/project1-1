60480ad00bc60edfe3dc35d3ff6becc31796796d
Mary Griffus <mary.e.shultz@gmail.com>
2016-03-27 10:02:23 -0400

    added pseudocode, added files for components

---
+++ conc-script.js
@@ -0,0 +1,14 @@
+// pseudocode - concentration
+
+// populate cards
+// display cards
+
+// rounds:
+//  player chooses first card
+//  player chooses second card
+//  check for match
+//   if no match: wait 2 seconds, flip back over
+//   else do nothing
+// if all matches are made, leave round loop. otherwise, restart
+
+// begin new game
