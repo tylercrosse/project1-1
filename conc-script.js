17f74293cb4997a16b192888e3ed068cb63040e5
Mary Griffus <mary.e.shultz@gmail.com>
2016-03-28 09:06:28 -0400

    added populate and shuffle deck functions

--- conc-script.js
+++ conc-script.js
@@ -1,6 +1,37 @@
 // pseudocode - concentration
 
+// lists of potential card faces
+var shapes = ['triangle', 'square', 'circle', 'parallelogram', 'pentagon', 'octagon', 'hexagon', 'rectangle'];
+
+// deck variables
+var numCards = 16;
+var numSymbols = numCards / 2;
+var deckRaw = [];
+var deck = [];
+deckTheme = shapes;
+
 // populate cards
+function populateCards() {
+  for (var i = 0; i < numSymbols; i++){
+    deckRaw.push(deckTheme[i]);
+    deckRaw.push(deckTheme[i]);
+  }
+}
+
+// shuffle cards
+function shuffleCards() {
+  i = 0
+  while (i < deckRaw.length){
+    var newCard = parseInt(Math.random() * deckRaw.length)
+    deck.push(deckRaw[newCard]);
+    deckRaw.splice(newCard, 1);
+  }
+}
+
+populateCards();
+shuffleCards();
+console.log(deck);
+
 // display cards
 
 // rounds:
