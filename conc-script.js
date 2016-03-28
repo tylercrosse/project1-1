a8c1808c7e37ac329371f1206e6720304beca066
Mary Griffus <mary.e.shultz@gmail.com>
2016-03-28 16:21:53 -0400

    fixed toggling issue

--- conc-script.js
+++ conc-script.js
@@ -32,11 +32,11 @@ concentration = {
     instButton = document.querySelector(".inst-container button");
     instButton.addEventListener("click", function() {
       instText = document.querySelector(".inst-container p");
-      if (instText.style.visibility == "hidden"){
-        instText.style.visibility = "visible";
+      if (instText.style.visibility == "visible"){
+        instText.style.visibility = "hidden";
       }
       else {
-        instText.style.visibility = "hidden";
+        instText.style.visibility = "visible";
       }
     })
   },
